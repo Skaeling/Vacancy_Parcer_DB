343afe2a76a531a9799c44f9e4319acf48dc1658
Lana Virina <lana.virina@gmail.com>
2024-10-11 14:01:12 +0300

    FIX API class

--- src/API_classes.py
+++ src/API_classes.py
@@ -1,39 +1,26 @@
-from abc import ABC, abstractmethod
-from itertools import chain
-
-import requests
 from typing import Any
 
-VACANCY_COUNT = 100
-PAGE_COUNT = 10
+import requests
 
 
-class API(ABC):
-    @abstractmethod
-    def get_request(self, url, params):
-        pass
+class HHRequest:
+    """Класс для получения информации от HHunter API"""
 
-    @abstractmethod
-    def get_vacancies(self, keyword):
-        pass
-
-
-class HHVacancy(API):
-    """Класс для взаимодействия с HHunter API"""
     def __init__(self, employee_ids: list):
         self.employee_ids = employee_ids
-        self.url = 'https://api.hh.ru/employers/'
+        self.url = "https://api.hh.ru/employers/"
 
-    def get_request(self, url, params=None) -> list[dict[str, Any]]:
-        headers = {'User-Agent': 'HH-User-Agent'}
+    @staticmethod
+    def get_request(url: str, params: dict[Any, Any] | None = None) -> Any:
+        """Выполняет запрос к API в соответствии с заданными параметрами"""
+        headers = {"User-Agent": "HH-User-Agent"}
         response = requests.get(url, headers=headers, params=params)
         if response.status_code == 200:
-            if 'salary' in response.text:
-                data = response.json()['items']
+            if "salary" in response.text:
+                data = response.json()["items"]
             else:
                 data = response.json()
             return data
-
         else:
             print("Error:", response.status_code)
 
@@ -41,16 +28,28 @@ class HHVacancy(API):
         """По employer_id возвращает список с основными данными по работодателю"""
         emp_data = []
         for employer_id in self.employee_ids:
-            url = f'{self.url+employer_id}'
+            url = f"{self.url + employer_id}"
             response = self.get_request(url)
             emp_data.append(response)
         return emp_data
 
-    def get_vacancies(self, emp_data: list) -> list:
-        """По employer_id возвращает список вакансий заданных работодателей"""
+    def get_vacancies(self, emp_data: dict) -> list:
+        """По employer_id возвращает список открытых вакансий (не более 500 от каждого работодателя),
+        с обязательным указанием зп"""
         vac_data = []
-        for i in emp_data:
-            params = {'text': '', 'page': 0, 'per_page': 10, 'employer_id': i["id"], 'only_with_salary': 'true'}
-            response = self.get_request(i["vacancies_url"], params)
-            vac_data.append(response)
-        return list(chain.from_iterable(vac_data))
+        page_count = 1
+        if 100 < emp_data["open_vacancies"] < 500:
+            page_count = emp_data["open_vacancies"] // 100
+        elif emp_data["open_vacancies"] >= 500:
+            page_count = 5
+        for i in range(page_count):
+            params = {
+                "text": "",
+                "page": i,
+                "per_page": 100,
+                "employer_id": emp_data["id"],
+                "only_with_salary": "true",
+            }
+            response = self.get_request(emp_data["vacancies_url"], params)
+            vac_data.extend(response)
+        return vac_data
