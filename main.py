de65ae9156f5dae65790acd5f97ea99d0046a98c
Lana Virina <lana.virina@gmail.com>
2024-10-11 14:02:16 +0300

    Update user interactive

--- main.py
+++ main.py
@@ -1,53 +1,86 @@
+from src.API_classes import HHRequest
 from src.config import config
-from src.utils import create_database, user_interactive
-from src.DB_classes import DBManager
-from src.API_classes import HHVacancy
+from src.DB_classes import DBCreator, DBManager
+from src.utils import create_database, represent_results
 
 
 def main():
-    #  Получение информации по работодателям
-    #  и предлагаемым ими вакансиям из списка 10 работодателей
+    print(
+        "\nПриветствуем вас в подборе вакансий! \n\n"
+        "Доступные действия: \n"
+        "1. Получить перечень 10 компаний-работодателей и их открытых вакансий\n"
+        "2. Просмотреть все доступные вакансии\n"
+        "3. Узнать среднюю зарплату по открытым вакансиям\n"
+        "4. Просмотреть список всех вакансий с заработной платой выше среднего\n"
+        "5. Подобрать вакансии по ключевому слову\n"
+        "6. Завершить подбор\n"
+    )
+    user_answer = input("Введите номер опции, чтобы отправить запрос: ")
 
-    employers_ids = ['1122462',
-                     '1740'
-                     ]
-    emp_clas = HHVacancy(employers_ids)
-    employers_data = emp_clas.get_employers()
-    vacancies_data = emp_clas.get_vacancies(employers_data)
-    # print(ids)
-    # print(vacs)
-
-    #  Создание БД для хранения полученной из API информации
-    params = config()
-    create_database('hh', params)
-
-    #  Создание таблиц и загрузка в них данных
-    db = DBManager('hh', params, employers_data, vacancies_data)
-    db.save_emp_to_database()
-    db.save_vacancy_to_database()
-
-    print('\nПриветствуем вас в парсере вакансий! \n'
-          'Введите номер опции чтобы отправить запрос:\n')
-    print('1. Получить перечень 10 доступных компаний и их открытых вакансий\n'
-          '2. Просмотреть все доступные вакансии\n'
-          '3. Узнать среднюю зарплату доступных вакансий\n'
-          '4. Узнать список всех вакансий с заработной платы выше среднего\n'
-          '5. Подобрать вакансии по ключевому слову\n'
-          '6. Завершить подбор\n')
-    user_answer = input()
-    db_methods = {'1': db.get_companies_and_vacancies_count(), '2': db.get_all_vacancies(),
-                  '3': db.get_avg_salary(), '4': db.get_vacancies_with_higher_salary()}
-    if user_answer not in '123456':
-        print("Нет такой категории")
+    if user_answer not in "123456":
+        print("Нет такой категории, пожалуйста, выберите другую")
         main()
-    elif user_answer == '6':
+    elif user_answer == "6":
         quit()
-    elif user_answer == '5':
-        user_keyword = input('Введите слово для поиска вакансии ').title()
-        search_vacancy = db.get_vacancies_with_keyword(user_keyword)
-        user_interactive(user_answer, search_vacancy, user_keyword)
     else:
-        user_interactive(user_answer, db_methods.get(user_answer))
+        # Отправка запроса к API
+        employers_ids = [
+            "5569859",
+            "6093775",
+            "64174",
+            "4934",
+            "2324020",
+            "3776",
+            "3331449",
+            "3769",
+            "370421",
+            "999442",
+        ]
+        api_hh = HHRequest(employers_ids)
+        employers_data = api_hh.get_employers()
+        vacancies_data = []
+        for v in employers_data:
+            vacancies_data.extend(api_hh.get_vacancies(v))
+
+        #  Создание БД для хранения полученной из API информации
+        params = config()
+        create_database("hh", params)
+
+        #  Создание таблиц и загрузка в них данных
+        db = DBCreator("hh", params, employers_data, vacancies_data)
+
+        #  Получение данных из таблиц и презентация пользователю
+        hh = DBManager("hh", params, employers_data, vacancies_data)
+
+        if user_answer == "5":
+            user_keyword = input("Введите ключевое слово для поиска вакансии ")
+            search_vacancy = hh.get_vacancies_with_keyword(user_keyword)
+            if not search_vacancy:
+                print("\nК сожалению, ничего не найдено")
+                user_choice = input("Начать новый поиск? (да/нет) ").lower()
+                if user_choice == "да":
+                    db.close_db()
+                    hh.close_db()
+                    main()
+                else:
+                    quit()
+            else:
+                represent_results(user_answer, search_vacancy, user_keyword)
+        else:
+            hh_methods = {
+                "1": hh.get_companies_and_vacancies_count(),
+                "2": hh.get_all_vacancies(),
+                "3": hh.get_avg_salary(),
+                "4": hh.get_vacancies_with_higher_salary(),
+            }
+            represent_results(user_answer, hh_methods.get(user_answer))
+            new_search = input("Направить новый запрос? (да/нет) ").lower()
+            if new_search == "да":
+                db.close_db()
+                hh.close_db()
+                main()
+            else:
+                quit()
 
 
 if __name__ == "__main__":
--- src/utils.py
+++ src/utils.py
@@ -1,32 +1,38 @@
+import pandas as pd
 import psycopg2
+from tabulate import tabulate
 
 
 def create_database(database_name: str, params: dict) -> None:
     """Создание базы данных и таблиц для сохранения данных о работодателях и вакансиях"""
-    conn = psycopg2.connect(dbname='postgres', **params)
+    conn = psycopg2.connect(dbname="postgres", **params)
     conn.autocommit = True
     cur = conn.cursor()
 
-    cur.execute(f'DROP DATABASE IF EXISTS {database_name}')
-    cur.execute(f'CREATE DATABASE {database_name}')
+    cur.execute(f"DROP DATABASE IF EXISTS {database_name}")
+    cur.execute(f"CREATE DATABASE {database_name}")
 
     cur.close()
     conn.close()
 
 
-def user_interactive(user_answer: str, db_data: list, keyword=None):
-    """Возвращает ответ пользователю по полученному номеру вопроса"""
-    if user_answer == '1':
-        msg = f"|Работодатели и количество открытых вакансий|"
-    elif user_answer == '2':
-        msg = f'|Доступные для просмотра вакансии|'
-    elif user_answer == '3':
-        msg = f'|Средняя зарплата по всем вакансиям и работодателям|'
-    elif user_answer == '4':
-        msg = f'|Перечень вакансий с заработной платой выше среднего|'
+def represent_results(user_answer: str, db_data: list, keyword: str | None = None) -> None:
+    """Возвращает ответ пользователю по полученному номеру запроса"""
+    if user_answer == "1":
+        msg = f">>> Работодатели и количество открытых вакансий"
+        headings = ["Компания", "Количество открытых вакансий"]
+        df = pd.DataFrame(db_data, columns=headings)
+    elif user_answer == "3":
+        msg = f">>> Средняя зарплата по всем вакансиям и работодателям"
+        headings = ["Компания", "Средняя заработная плата", "Валюта"]
+        df = pd.DataFrame(db_data, columns=headings)
     else:
-        if not db_data:
-            msg = "К сожалению, ничего не найдено"
+        headings = ["Компания", "Вакансия", "ЗП от", "ЗП до", "Валюта", "Cсылка на вакансию"]
+        df = pd.DataFrame(db_data, columns=headings)
+        if user_answer == "2":
+            msg = f">>> Доступные для просмотра вакансии"
+        elif user_answer == "4":
+            msg = f">>> Перечень вакансий с заработной платой выше среднего"
         else:
-            msg = f'>>>Результаты поиска по слову: "{keyword}"\n'
-    return print(msg), [print(d) for d in db_data]
+            msg = f'>>>Результаты поиска по слову: "{keyword}"'
+    return print(f"\n{msg}\n{tabulate(df, headers='keys', tablefmt='psql')}")
